9ecd3b8d98675f30b68c21de7a004c83f860f2b0
Bradley Bossard <bradleybossard@gmail.com>
2016-11-05 22:14:26 +0000

    Use version from package.json

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 const fs = require('fs');
 const program = require('commander');
 const fauna = require('fauna');
+const pkg = require('./package.json');
 
 function runFauna(input, output) {
   const config = JSON.parse(fs.readFileSync(input, 'utf8'));
@@ -17,7 +18,7 @@ function runFauna(input, output) {
 };
 
 program
-  .version('1.0.0')
+  .version(pkg.version)
   .arguments('<input> <output>')
   .usage('<input> <output>\n\n  where <input>=/path/to/input/config\n        <output>=/path/to/output/svg')
   .description('CLI tool for fauna library for generating animated L-Systems')
