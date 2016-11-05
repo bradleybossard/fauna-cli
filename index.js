670263557bca9ce831d2a147e0b47075c3a8ec87
Bradley Bossard <bradleybossard@gmail.com>
2016-11-05 16:02:43 +0000

    Add input/output params and validation

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 #!/usr/bin/env node
 
-console.log('Hello world');
+'use strict';
+const program = require('commander');
+program
+  .version('1.0.0')
+  .usage('--input <input> --output <output>')
+  .description('CLI tool for fauna library for generating animated L-Systems')
+  .option('-i, --input <input-config>','path in input config json')
+  .option('-o, --output <output-svg>', 'path to output svg file')
+  //.action('');
+  .parse(process.argv);
+
+if (!program.input) {
+  console.log('');
+  console.log('  --input parameter required');
+  program.help();
+}
+
+if (!program.output) {
+  console.log('');
+  console.log('  --output parameter required');
+  program.help();
+}
 
